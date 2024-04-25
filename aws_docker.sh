e87759a20ca0c8b20029a768f5e17e3f0341c8c4
Angel Lopez <109396828+GLopezAngel@users.noreply.github.com>
2024-04-25 10:23:30 -0400

    Add comments to aws_docker.sh

--- aws_docker.sh
+++ aws_docker.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
+# Downloads, enables, and starts docker on the EC2 Instance
 sudo yum install -y docker
 sudo systemctl enable docker
 sudo systemctl start docker
+# Allow access to the ec2 user
 sudo usermod -a -G docker ec2-user
 
-
+echo "*********************************************************"
 echo "**Log out and log back in, and then run ./dockstart.sh.**"
+echo "*********************************************************"
